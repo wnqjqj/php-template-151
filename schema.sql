db01abb364839a7f6250c4afe13752bda834bed3
wnqjqj <wnstl82@naver.com>
2017-06-19 11:16:32 +0200

    Update

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
+-- Adminer 4.2.5 MySQL dump
+
+USE app;
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
-
-use app;
 
 DROP TABLE IF EXISTS `blog_members structure`;
 CREATE TABLE `blog_members structure` (
@@ -25,4 +26,14 @@ CREATE TABLE `blog_posts structure` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
--- 2017-06-12 07:57:50
+DROP TABLE IF EXISTS `User`;
+CREATE TABLE `User` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `User` (`id`, `email`, `password`) VALUES
+(0,	'wnqjqj@naver.com',	'a1673611');
+
+-- 2017-06-19 09:12:19
